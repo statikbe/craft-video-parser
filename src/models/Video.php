de4b5c6eceacdc4bf0cb736ede45c5434bc67f5c
Jan Henckens <jan@statik.be>
2022-12-22 09:59:17 +0100

    Playlist support for Craft 3
    Signed-off-by: Jan Henckens <jan@statik.be>

--- src/models/Video.php
+++ src/models/Video.php
@@ -7,8 +7,10 @@ use craft\base\Model;
 class Video extends Model
 {
     const TYPE_YOUTUBE = 'youtube';
+    const TYPE_YOUTUBE_PLAYLIST = 'youtube_playlist';
     const TYPE_VIMEO = 'vimeo';
     const BASE_YOUTUBE = "https://www.youtube.com/embed/";
+    const BASE_YOUTUBE_PLAYLIST = "https://www.youtube.com/embed/videoseries?list=";
     const BASE_YOUTUBE_NOCOOKIES = "https://www.youtube-nocookie.com/embed/";
     const BASE_VIMEO = "https://player.vimeo.com/video/";
 
@@ -28,16 +30,25 @@ class Video extends Model
         if (strpos($url, 'youtu')) {
             $this->type = Video::TYPE_YOUTUBE;
             if (!function_exists('str_contains')) {
-                function str_contains($haystack, $needle) {
+                function str_contains($haystack, $needle)
+                {
                     return $needle !== '' && mb_strpos($haystack, $needle) !== false;
                 }
             }
-            if(str_contains($url, 'nocookie')) {
+            if (str_contains($url, 'nocookie')) {
                 $this->noCookies = true;
             }
-            preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);
-            $this->id = $match[1];
-            $this->getEmbedSrc();
+
+            if (str_contains($url, 'playlist')) {
+                $this->type = self::TYPE_YOUTUBE_PLAYLIST;
+                preg_match("/[&?]list=([^&]+)/i", $url, $match);
+                $this->id = $match[1];
+                $this->getEmbedSrc();
+            } else {
+                preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)|shorts/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);
+                $this->id = $match[1];
+                $this->getEmbedSrc();
+            }
         } elseif (strpos($url, 'vimeo')) {
             $this->type = Video::TYPE_VIMEO;
             preg_match('(https?:\/\/(?:[\w]+\.)*vimeo\.com(?:[\/\w:]*(?:\/videos)?)?\/([0-9]+)[^\s]*)', $url, $match);
@@ -48,9 +59,11 @@ class Video extends Model
 
     private function getEmbedSrc()
     {
-        if ($this->type === self::TYPE_YOUTUBE) {
-            if($this->noCookies) {
+        if ($this->type === self::TYPE_YOUTUBE || $this->type === self::TYPE_YOUTUBE_PLAYLIST) {
+            if ($this->noCookies) {
                 $this->embedSrc = self::BASE_YOUTUBE_NOCOOKIES . $this->id;
+            } elseif ($this->type === self::TYPE_YOUTUBE_PLAYLIST) {
+                $this->embedSrc = self::BASE_YOUTUBE_PLAYLIST . $this->id;
             } else {
                 $this->embedSrc = self::BASE_YOUTUBE . $this->id;
             }
