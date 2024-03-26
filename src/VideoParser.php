5e834f3a0532cd19a66812d51678fc889cb55eac
Jan Henckens <jan@statik.be>
2024-03-26 20:54:56 +0100

    ECS fixes

--- src/VideoParser.php
+++ src/VideoParser.php
@@ -4,24 +4,22 @@ namespace statikbe\videoparser;
 
 use craft\base\Plugin;
 use craft\web\twig\variables\CraftVariable;
-use modules\statik\variables\StatikVariable;
 use statikbe\videoparser\variables\VideoParserVariable;
 use yii\base\Event;
 
 class VideoParser extends Plugin
 {
-	 public function init()
+    public function init()
     {
         parent::init();
         Event::on(
             CraftVariable::class,
             CraftVariable::EVENT_INIT,
-            function (Event $event) {
+            function(Event $event) {
                 /** @var CraftVariable $variable */
                 $variable = $event->sender;
                 $variable->set('videoparser', VideoParserVariable::class);
             }
         );
-
     }
 }
